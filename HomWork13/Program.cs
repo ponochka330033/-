882b6a9d2018f9b52c06d5552ebe48805a733cc4
Helen <ponochka330033@yandex.ru>
2023-05-26 20:53:16 +0300

    Задача 13

---
+++ HomWork13/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int Number = int.Parse (userInput);
+int Num = 0;
+
+if (Number >= 100)
+{
+    while (Number > 999)
+    {
+        Number = Number / 10;
+    }
+    Num = Number % 10;
+    Console.WriteLine($"{Number} -> {Num}");
+}
+else 
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
+
