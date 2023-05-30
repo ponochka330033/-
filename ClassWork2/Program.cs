7e3e84ac87edb0a4686842cf50ed735205f15c75
Helen <ponochka330033@yandex.ru>
2023-05-30 20:45:37 +0300

    Задача 2

---
+++ ClassWork2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+Console.Clear();
+string mess = "Введите число: ";
+int num = Prompt(mess);
+//int summ = GetSumNums(num);
+Console.WriteLine($"Сумма равна = {GetSumNums(num)}");
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+        Console.Write(sum + " ");
+    }
+    return sum;
+}
