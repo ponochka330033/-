2b17aa05e5d175b5fa2e98b8f88648ed86a1dc7d
Helen <ponochka330033@yandex.ru>
2023-05-30 19:19:00 +0300

    Задача 19

---
+++ HomWork19/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+string EnteringNumber(string message)
+{
+    Console.Clear();
+    string Num = Console.ReadLine()!;
+    return(Num);
+}
+
+string palindrome = EnteringNumber("Введите пятизначное число: ");
+Palindrome (palindrome);
+
+
+void Palindrome(string pal)
+{
+    int count = pal.Length;
+
+    if (count == 5)
+    {
+        if (pal[0] == pal[4] && pal[1] == pal[3])
+        {
+            Console.WriteLine ("Палиндром");
+        }
+        else
+        {
+            Console.WriteLine ("Не палиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine ("Ошибка ввода");
+    }
+}
