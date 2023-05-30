edf6cdf91a78160cbcec5970f55f4af6ba12ba4f
Helen <ponochka330033@yandex.ru>
2023-05-30 23:24:27 +0300

    Задача 29

---
+++ HomWork29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[]arr = {1,2,5,7,19,6,1,33};
+PrintArray(arr);
+
+void PrintArray(int[]Coll)
+{
+    int count = Coll.Length;
+    int possition = 0;
+    while(possition < count)
+    {
+        Console.Write(Coll[possition]+",");
+        possition++;
+    }
+}
+
