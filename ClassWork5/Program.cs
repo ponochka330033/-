2385f0533a548d69a2ee5a6325e84e05f30879af
Helen <ponochka330033@yandex.ru>
2023-05-30 22:05:53 +0300

    Задача 5

---
+++ ClassWork5/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[]array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = new Random().Next(0,2);
+        index++;
+    }
+}
+
+int[]arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
+
+void PrintArray(int[]Coll)
+{
+    int count = Coll.Length;
+    int possition = 0;
+    while(possition < count)
+    {
+        Console.Write(Coll[possition]+" ");
+        possition++;
+    }
+}
