d805f1a5511316ac809b556b430a7b6d7d58fd5f
Helen <ponochka330033@yandex.ru>
2023-06-04 19:19:23 +0300

    Задача 34

---
+++ HomWork34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] array = GetArray(4, 100, 1000);
+PrintArray(array);
+int count = ExamArr(array);
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел - {count}");
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+    return res;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+int ExamArr (int[] arr)
+{
+    int exam = 0;
+    for (int i=0;i<arr.Length;i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+        exam ++;
+        }  
+    }
+    return exam;
+}
