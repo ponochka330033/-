2c2ed9eac5886fcd948bf3f50e5b623e1f33d2b5
Helen <ponochka330033@yandex.ru>
2023-06-04 19:36:29 +0300

    Задача 36

---
+++ HomWork36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] array = GetArray(4, 1, 51);
+PrintArray(array);
+int sum = GetSumElements(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов - {sum}");
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+    return res;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int GetSumElements(int[] arr)
+{
+    int element = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            element = element + arr[i];
+        }
+    }
+    return element;
+}
