9748a608b6807cc30aa89787a68b1ab0981b184a
Helen <ponochka330033@yandex.ru>
2023-06-09 09:46:37 +0300

    Задача 41

---
+++ HomWork41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите элементы через пробел: ");
+int[]Number = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int size = Result(Number);
+Console.WriteLine($"Количество элементов > 0: {size}");
+
+int Result(int[]arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return(count);
+}
+
+
+
+
