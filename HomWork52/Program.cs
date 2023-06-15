42779386c68c063f56c9e302d44bdc364e9c641b
Helen <ponochka330033@yandex.ru>
2023-06-15 22:08:56 +0300

    Задача 52

---
+++ HomWork52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+
+int[,] array = GetArray(rows, columns, 1, 10);
+PrintArray(array);
+MeinColumn(rows, columns, array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) 
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MeinColumn(int m, int n, int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            float sum = 0;
+            float arithmetic = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sum = sum + arr[i,j];
+            }
+            arithmetic = sum / m;
+            Console.Write($"Среднее арифметическое каждого столбца = {arithmetic }; ");
+        }
+}
